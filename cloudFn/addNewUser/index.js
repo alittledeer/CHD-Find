49a4142363b8463f63ad6594f31fb0f5499d425d
alittledeer <50263976+alittledeer@users.noreply.github.com>
2020-09-18 14:59:34 +0800

    Update index.js

--- cloudFn/addNewUser/index.js
+++ cloudFn/addNewUser/index.js
@@ -4,15 +4,33 @@ cloud.init({
   env:"mychd-find-4s9do",
 })
 const ydb = cloud.database()
-//传入的值有id、newHome、storeTime
+
 // 云函数入口函数
 exports.main = async(event) => {
-      let res = await ydb.collection('CHDLost').doc(id).update({
-        // data 传入需要局部更新的数据
-        data: {
-          forShow: 1,
-          newHome: that.data.newHome,
-          storeTime: storeTime
-        }
-      })
+  //首先查询用户，没有用户自动注册用户
+  let user = null;
+  let res = await ydb.collection('user').where({
+    _openid: event.openid,
+  }).get()
+  console.log(res)
+  if (res.data.length == 0) {
+    //新增
+    let addNew = await ydb.collection('user').add({
+      data: {
+        _openid: event.openid,
+        nickName: event.name,
+        userImg: event.img,
+        isAdministrator: false,
+        isSellerExamine: 0,
+        phoneNumber: "",
+        studentID: "",
+        SFZID: "",
+        writeAllID: false
+      },
+    })
+    user = "new"
+  } else {
+    user = "old"
+  }
+  return user
 }
